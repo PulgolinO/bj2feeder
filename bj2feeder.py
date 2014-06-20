372275cc4da98acedd55fb734ea3f4f8410b8f44
pulgalipe <pulgalipe@users.noreply.github.com>
2014-06-20 20:17:25 -0300

    Update bj2feeder.py
    Fixed a bug where it don't download files with unicode characters.
    Corrigido bug onde não baixava arquivos com caracteres em unicode.

--- bj2feeder.py
+++ bj2feeder.py
@@ -1,4 +1,7 @@
-#Importa as bibliotecas necessarias
+# -*- coding: utf-8 -*-
+#!/usr/bin/python
+
+#Importa as bibliotecas necessárias
 import feedparser, urllib2, os, re
 
 #-----------------------------------#
@@ -11,7 +14,7 @@ os.chdir( '/home/nome_do_usuario' )
 #Tenha certeza que tem direito para escrever no diretório
 
 #Define os releases que deverão ser baixados
-#Não se esqueça de colocar o nome de cada release separado por vígula e dentro de aspas duplas
+#Não se esqueça de colocar o nome de cada release separado por vírgula e dentro de aspas duplas
 RELEASES = [ "Release1|Release2|Release3" ]
 #Ex: RELEASES = [ "Game.of.Thrones|Spiderman|Da.Vinci" ]
 
@@ -25,8 +28,8 @@ UID = "Sua_UID_do_BJ2" #Coloque aqui seu UID do BJ2. Não se esqueça de colocar
 #----NAO EDITE NADA A PARTIR DAQUI---#
 #------------------------------------#
 
-#Define a URL do Tracker que será acessado pelo feedparser
-#Utilize o método "feedparser.parse" sempre que precisar acessar um feed
+#Define a URL do Tracker que sera acessado pelo feedparser
+#Utilize o metodo "feedparser.parse" sempre que precisar acessar um feed
 bjurl = 'http://bj2.me/rss.php'
 bjurl = feedparser.parse( bjurl )
 
@@ -41,7 +44,8 @@ for rel in range( len(RELEASES) ):
                 link = link.replace( 'detalhes', 'download' )
                 if re.search( RELEASES[ rel ], title, re.I ):
                         print( 'Baixando Torrent: ' + title )
+                        title = title.encode('ascii', 'ignore')
                         print( 'Link: ' + link )
-                        os.system( "wget -q --header \"Cookie: pass=" + str( PASS ) + ";uid=" + str( UID ) + "\" -O \"" + title + ".torrent\" " + link )
+                        os.system( "wget -q --header \"Cookie: pass=" + PASS + ";uid=" + UID + "\" -O \"" + title + ".torrent\" " + link )
                         print( 'Download concluido.\n' )
 print( 'Script finalizado com sucesso!' )
